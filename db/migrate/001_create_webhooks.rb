dd58953c4abbb8f56df8ab6b88269e5f5ef91a42
Karsa <contact@karsa.org>
2023-04-26 17:28:51 +0200

    Adds settings to add Authorization key + subproject inheritance

--- db/migrate/001_create_webhooks.rb
+++ db/migrate/001_create_webhooks.rb
@@ -1,7 +1,9 @@
 class CreateWebhooks < ActiveRecord::Migration[4.2]
   def change
     create_table :webhooks do |t|
-      t.string :url
+      t.string :url, :null => false
+      t.string :key, :null => true
+      t.boolean :subprojects, :default => :yes
       t.integer :project_id
     end
   end
--- lib/redmine_webhook/webhook_listener.rb
+++ lib/redmine_webhook/webhook_listener.rb
@@ -8,13 +8,26 @@ module RedmineWebhook
       false
     end
 
+    def get_parent_webhooks(project)
+      return unless project.parent
+      t = Webhook.table_name
+      webhooks = Webhook.where("#{t}.project_id = ? AND #{t}.subprojects = ?", project.parent.id, true)
+      webhooks = get_parent_webhooks(project.parent) unless webhooks && webhooks.length > 0
+      webhooks
+    end
+
+    def get_webhooks(project)
+      webhooks = Webhook.where(:project_id => project[:id])
+      webhooks = get_parent_webhooks(project) unless webhooks && webhooks.length > 0
+      webhooks = Webhook.where(:project_id => 0) unless webhooks && webhooks.length > 0
+      webhooks
+    end
+
     def controller_issues_new_after_save(context = {})
       return if skip_webhooks(context)
       issue = context[:issue]
       controller = context[:controller]
-      project = issue.project
-      webhooks = Webhook.where(:project_id => project.project.id)
-      webhooks = Webhook.where(:project_id => 0) unless webhooks && webhooks.length > 0
+      webhooks = get_webhooks(issue.project)
       return unless webhooks
       post(webhooks, issue_to_json(issue, controller))
     end
@@ -24,9 +37,7 @@ module RedmineWebhook
       journal = context[:journal]
       controller = context[:controller]
       issue = context[:issue]
-      project = issue.project
-      webhooks = Webhook.where(:project_id => project.project.id)
-      webhooks = Webhook.where(:project_id => 0) unless webhooks && webhooks.length > 0
+      webhooks = get_webhooks(issue.project)
       return unless webhooks
       post(webhooks, journal_to_json(issue, journal, controller))
     end
@@ -36,9 +47,7 @@ module RedmineWebhook
       journal = context[:journal]
       controller = context[:controller]
       issue = context[:issue]
-      project = issue.project
-      webhooks = Webhook.where(:project_id => project.project.id)
-      webhooks = Webhook.where(:project_id => 0) unless webhooks && webhooks.length > 0
+      webhooks = get_webhooks(issue.project)
       return unless webhooks
       post(webhooks, journal_to_json(issue, journal, controller))
     end
@@ -46,8 +55,7 @@ module RedmineWebhook
     def model_changeset_scan_commit_for_issue_ids_pre_issue_update(context = {})
       issue = context[:issue]
       journal = issue.current_journal
-      webhooks = Webhook.where(:project_id => issue.project.project.id)
-      webhooks = Webhook.where(:project_id => 0) unless webhooks && webhooks.length > 0
+      webhooks = get_webhooks(issue.project)
       return unless webhooks
       post(webhooks, journal_to_json(issue, journal, nil))
     end
@@ -80,6 +88,7 @@ module RedmineWebhook
           begin
             Faraday.post do |req|
               req.url webhook.url
+              req.headers['Authorization'] = "Bearer #{webhook.key}" if webhook.key
               req.headers['Content-Type'] = 'application/json'
               req.body = request_body
             end
